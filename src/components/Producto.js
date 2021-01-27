7e99a23a4b367e10fe1cfbcb9f9d3229ad906fac
sarastackhouse <sarapalacios7@gmail.com>
2021-01-27 19:04:29 +0100

    Done without styles

--- src/components/Producto.js
+++ src/components/Producto.js
@@ -4,20 +4,34 @@ const Producto = ({ producto, carrito, agregarProducto, productos }) => {
   const { nombre, precio, id } = producto;
 
   // agregar producto al carrito
-  const seleccionarProducto = id => {
-    const producto = productos.filter(producto => producto.id === id)[0]
-    agregarProducto([
-        ...carrito, producto
-    ]);
-    };
+  const seleccionarProducto = (id) => {
+    const producto = productos.filter((producto) => producto.id === id)[0];
+    agregarProducto([...carrito, producto]);
+  };
+
+  // eliminar producto del carrito
+  const eliminarProducto = (id) => {
+    const productos = carrito.filter((producto) => producto.id !== id);
+
+    //Colocar los productos en el state
+
+    agregarProducto(productos);
+  };
 
   return (
     <div>
       <h2>{nombre}</h2>
       <p>${precio}</p>
-      <button type="button" onClick={() => seleccionarProducto(id)}>
-        Comprar
-      </button>
+
+      {productos ? (
+        <button type="button" onClick={() => seleccionarProducto(id)}>
+          Comprar
+        </button>
+      ) : (
+        <button type="button" onClick={() => eliminarProducto(id)}>
+          Eliminar
+        </button>
+      )}
     </div>
   );
 };
